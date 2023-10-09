67287ad3602fa1066b11491b09ec5a49feb6fc46
Julio Yorg <141753947+JulioYorg83@users.noreply.github.com>
2023-10-09 06:27:55 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/hotelgrupo30/Hotelgrupo30.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelgrupo30;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Hotelgrupo30 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
